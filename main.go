e35d6d51b2e53bb299b4a94becb675f57cb2f2cc
David Shaw <dshaw11@utk.edu>
2025-04-07 16:13:06 -0400

    reading 8 bytes

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	b := make([]byte, 8)
+
+	for {
+		val, err := f.Read(b)
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("read: %s\n", b[:val])
+	}
+
 }
