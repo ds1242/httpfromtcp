f1fc4f3a786c1d5b3f68ed6c8e39265fab9828c6
David Shaw <dshaw11@utk.edu>
2025-04-10 12:57:56 -0400

    created a channel, this is so confusing

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"strings"
 )
 
 const filePath = "messages.txt"
@@ -17,43 +14,14 @@ func main() {
 		log.Fatalf("could not open %s: %s\n", filePath, err)
 	}
 
-	defer f.Close()
-
 	fmt.Printf("Reading data from %s\n", filePath)
 	fmt.Println("===============================")
 
-	ch := getLinesChannel(file)
+	ch := getLinesChannel(f)
 	for line := range ch {
 		fmt.Printf("read: %s\n", line)
 	}
 
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	currentLine := ""
-	for {
 
-		buffer := make([]byte, 8, 8)
-		n, err := f.Read(buffer)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fullLine := currentLine + parts[i]
-			currentLine = ""
-			return fullLine
-		}
-		currentLine += parts[len(parts)-1]
-	}
-}
