4f9d1600aa4329fb5c60b1001267a481bb41b3ec
David Shaw <dshaw11@utk.edu>
2025-04-10 13:00:00 -0400

    moved func back to main

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"errors"
+	"strings"
+	"io"
 )
 
 const filePath = "messages.txt"
@@ -17,11 +20,47 @@ func main() {
 	fmt.Printf("Reading data from %s\n", filePath)
 	fmt.Println("===============================")
 
-	ch := getLinesChannel(f)
-	for line := range ch {
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
 		fmt.Printf("read: %s\n", line)
 	}
 
 }
 
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+	
+		currentLine := ""
+		for {
+
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					ch <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- currentLine + parts[i]
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	
+	}()
+	return ch
+}
