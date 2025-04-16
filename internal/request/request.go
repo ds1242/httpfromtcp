3d4ad2967a4386ea749a0f57b555afcb4c183ee9
David Shaw <dshaw11@utk.edu>
2025-04-16 11:35:48 -0400

    setup test and stubbed out the requestfromheader func

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,2 +1,15 @@
 package request
 
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+
+}
